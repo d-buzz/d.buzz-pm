25d3351076f8691a41df40fed8740a5466d762a8
stinkymonkeyph <monkeydev.team@gmail.com>
2020-11-13 00:57:57 +0800

    introduce material-ui

--- components/clock.js
+++ components/clock.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import Typography from '@material-ui/core/Typography'
 
 const formatTime = (time) => {
   return new Date(time).toJSON().slice(11, 19)
@@ -8,22 +9,26 @@ const Clock = () => {
   const lastUpdate = useSelector((state) => state.timer.lastUpdate)
   const light = useSelector((state) => state.timer.light)
   return (
-    <div className={light ? 'light' : ''}>
-      {formatTime(lastUpdate)}
-      <style jsx>{`
-        div {
-          padding: 15px;
-          display: inline-block;
-          color: #82fa58;
-          font: 50px menlo, monaco, monospace;
-          background-color: #000;
-        }
+    <React.Fragment>
+       <Typography variant='h2'>Hello World</Typography>
+      <div className={light ? 'light' : ''}>
+      
+        {formatTime(lastUpdate)}
+        <style jsx>{`
+          div {
+            padding: 15px;
+            display: inline-block;
+            color: #82fa58;
+            font: 50px menlo, monaco, monospace;
+            background-color: #000;
+          }
 
-        .light {
-          background-color: #999;
-        }
-      `}</style>
-    </div>
+          .light {
+            background-color: #999;
+          }
+        `}</style>
+      </div>
+    </React.Fragment>
   )
 }
 
